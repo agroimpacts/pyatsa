d30c4e91951eccbd74242ef4cce712d513c56342
Ryan Avery <ravery@ucsb.edu>
2019-07-22 23:09:47 -0700

    updated readme, install, and cleaned up code with linitng and deleting unused code

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # pyATSA
+pyatsa finds clouds in satellite imagery.
 
-pyatsa contains a working package that ports the original ATSA method and code by Zhu and Helmer 2018. Currently the python version produces nearly equivalent results for Planetscope 4-band images that do not contain lots of bright impervious surface, bright soils, or water.
+This repo contains the pyatsa package that ports the original ATSA method and code by Zhu and Helmer 2018, as well as example notebooks. Currently the python version produces nearly equivalent results to Planet Lab's Usable Data Mask for Planetscope 4-band images across Ghana. In cases where there are lots of bright impervious surface, bright soils, or water, pyatsa generates substantial false positives in the cloud shadow class and speckles of false positives from clouds. These are the primary issues being worked on.
 
-To compare pyatsa to the original IDL version, distributed by Zhu at https://xiaolinzhu.weebly.com/open-source-code.html, it is necessary to have IDL and ENVI installed. then:
+# Install
+
+`pip install pyatsa`
+
+## Install from source with dependencies
+
+```
+conda env create -f environment.yaml
+conda activate pyatsa
+# or anaconda3 if you use anaconda or whatever the path to your python file is
+flit install --symlink --python ~/miniconda3/envs/pyatsa/bin/python3.7
+```
+
+## Differences between pyatsa and original ATSA by Zhu and Helmer 2018
+
+- When computing the Clear Sky Lines for images in a time series, Zhu set A_cloud within .5-1.5 idea, but low values lead to too many false positives in Planetscope imagery.
+- I chose to use Li's cross entrpy thresholding method in scikit-image rather than Inverse Distance Weighting to segment cloud shadows within the potential cloud shadow zone calculated from the scene geometry. This was simpler to code and appeared to give better results, the IDW method had a strong bias toward only masking shadows directly adjacent to clouds. I observed cloud shadows that were almost a kilometer away from their cloud shadows throughout Ghana, where pyatsa is being tested. More actual shadows are correctly masked, though more false positives are generated where clouds are not present.
+
+
+# Comparing with the original ATSA IDL code
+The original ATSA program is distributed by the authors at https://xiaolinzhu.weebly.com/open-source-code.html. To run, it is necessary to have IDL and ENVI installed. then:
 
   1. Start the idl ide by calling `idlde` in the terminal
 
@@ -17,11 +38,10 @@ To compare pyatsa to the original IDL version, distributed by Zhu at https://xia
 The IDL script will save out a file that ends in ".sav" (you need to edit the script to change the output path of this file on your computer). It will contain all the variables and results from the script. The file can be read with `scipy.io.readsav` as a python dictionary where each key is the origianl idl variable name. This can then be used in the pytests to check for agreement.
 
 To run tests, navigate to the atsa-python folder and run `pytest test_pyatsa.py`. pytest will check for any functions that contain the word "test" and execute that function, producing times and test results for each test function. pytest fixtures are used to define variables that are shared between tests. These tests currently use two fixtures, one for the configsfor the python version (which are copied from the idl version) and a fixture for the ~3Gb .sav file that contains all the variables from the idl version.
- 
 
-#### Notes on the IDL version
+#### Notes on the IDL version to assist with debugging
 
-The IDL code has separate for loops that operate on water regions and land regions.
+The IDL code has separate for loops that operate on water regions (identified by a pre-existing water mask) and land regions.
 
 The water mask is 0 value where water, 1 where there is not water. The mask values for land classes are as follows.
 
--- pyatsa/pyatsa_configs.py
+++ pyatsa/pyatsa_configs.py
@@ -1,39 +1,46 @@
 import numpy as np
 import skimage.io as skio
-import os
+
 
 class ATSA_Configs():
     def __init__(self, image_path, angles_path):
-        self.t_series=skio.imread(image_path)
+        self.t_series = skio.imread(image_path)
         self.angles = np.genfromtxt(angles_path, delimiter=' ')
-        #set the following parameters
-        self.dn_max=10000  #maximum value of DN, e.g. 7-bit data is 127, 8-bit is 255
-        self.background=0  #DN value of background or missing values, such as SLC-off gaps
-        self.buffer=1    #width of buffer applied to detected cloud and shadow, recommend 1 or 2 
+        # set the following parameters
+        self.dn_max = 10000  # maximum value of DN, e.g. 7-bit data is 127, 8-bit is 255
+        self.background = 0  # DN value of background or missing values, such as SLC-off gaps
+        self.buffer = 1  # width of buffer applied to detected cloud and shadow, recommend 1 or 2
 
-        #parameters for HOT caculation and cloud detection
-        #------------------------------
-        self.n_band=4     # number of bands of each image
-        self.n_image=self.t_series.shape[2]/self.n_band   # number of images in the time-series
-        self.blue_b=0    # band index of blue band, note: MSS does not have blue, use green as blue
-        self.green_b=1   # band index of green band
-        self.red_b=2     # band index of red band
-        self.nir_b=3     # band index of nir band
+        # parameters for HOT caculation and cloud detection
+        # ------------------------------
+        self.n_band = 4     # number of bands of each image
+        # number of images in the time-series
+        self.n_image = self.t_series.shape[2]/self.n_band
+        self.blue_b = 0    # band index of blue band, note: MSS does not have blue, use green as blue
+        self.green_b = 1   # band index of green band
+        self.red_b = 2     # band index of red band
+        self.nir_b = 3     # band index of nir band
 
-        self.A_cloud=2.0 # threshold to identify cloud (mean+A_cloud*sd), recommend 0.5-1.5 for Landsat, smaller values can detect thinner clouds
-        self.maxblue_clearland=self.dn_max*0.15 # estimated maximum blue band value for clear land surface
-        self.maxnir_clearwater=self.dn_max*0.05 # estimated maximum nir band value for clear water surface
-        self.rmax = self.maxblue_clearland # max value for blue band for computing clear line
-        self.rmin = .01*self.dn_max # min DN value for blue band for computing clear line
-        self.n_bin = 50 # number of bins between rmin and rmax
+        # threshold to identify cloud (mean+A_cloud*sd), recommend 0.5-1.5 for Landsat, smaller values can detect thinner clouds
+        self.A_cloud = 2.0
+        # estimated maximum blue band value for clear land surface
+        self.maxblue_clearland = self.dn_max*0.15
+        # estimated maximum nir band value for clear water surface
+        self.maxnir_clearwater = self.dn_max*0.05
+        # max value for blue band for computing clear line
+        self.rmax = self.maxblue_clearland
+        self.rmin = .01*self.dn_max  # min DN value for blue band for computing clear line
+        self.n_bin = 50  # number of bins between rmin and rmax
 
-        #parameters for shadow detection
-        #------------------------------
-        self.shortest_d=7.0       #shortest distance between shadow and cloud, unit is pixel resolution
-        self.longest_d=300.0  #longest distance between shadow and its corresponding cloud, unit is "pixel",can be set empirically by inspecting images
-        self.B_shadow=1.5   #threshold to identify shadow (mean-B_shadow*sd), recommend 1-3, smaller values can detect lighter shadows
-        #------------------------------
+        # parameters for shadow detection
+        # ------------------------------
+        # shortest distance between shadow and cloud, unit is pixel resolution
+        self.shortest_d = 7.0
+        self.longest_d = 300.0  # longest distance between shadow and its corresponding cloud, unit is "pixel",can be set empirically by inspecting images
+        # threshold to identify shadow (mean-B_shadow*sd), recommend 1-3, smaller values can detect lighter shadows
+        self.B_shadow = 1.5
+        # ------------------------------
 
-        #we reshape our images that were stacked on the band axis into a 4D array
-        self.t_series = np.reshape(self.t_series,(self.t_series.shape[0],self.t_series.shape[1],self.n_band,int(self.n_image)), order='F')
-
+        # we reshape our images that were stacked on the band axis into a 4D array
+        self.t_series = np.reshape(
+            self.t_series, (self.t_series.shape[0], self.t_series.shape[1], self.n_band, int(self.n_image)), order='F')
