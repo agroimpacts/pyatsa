40102693474ad8fb5d7ae8cad3141380a09104ac
Ryan Avery <ravery@ucsb.edu>
2019-08-15 22:28:16 -0700

    plotting way better w/ xarray

--- pyatsa_explore.py
+++ pyatsa_explore.py
@@ -8,7 +8,7 @@ import time
 from pyatsa.tseries_preprocess import read_clean_tseries_as_xarr
 
 t_series = read_clean_tseries_as_xarr(
-    "/home/rave/pyatsa/cfg/buffered_imgs/ag-forestry/")
+    "/home/rave/pyatsa/cfg/buffered_imgs/city/")
 configs = configs.ATSA_Configs()
 
 reflectance = np.array(t_series['reflectance'])
@@ -49,7 +49,7 @@ for i in np.arange(refined_masks.shape[0]):
 print("seconds ", time.time()-start)
 print("finished cloud masking")
 
-# %%
+
 start = time.time()
 
 h_high, h_low = masking.cloud_height_min_max(
@@ -62,14 +62,14 @@ potential_shadow_masks = masking.make_potential_shadow_masks_multi(
 print("seconds ", time.time()-start)
 print("finished potential shadow masking")
 
-# %%
+
 start = time.time()
 
-clearest_land_nir, clearest_land_index = min_cloud_nir(
+clearest_land_nir, clearest_land_index = masking.min_cloud_nir(
     potential_shadow_masks, reflectance)
-gains_biases = gains_and_biases(
+gains_biases = masking.gains_and_biases(
     potential_shadow_masks, reflectance[:,:,:,3], clearest_land_nir, clearest_land_index)
-shadow_inds = shadow_index_land(
+shadow_inds = masking.shadow_index_land(
     potential_shadow_masks, reflectance[:,:,:,3], gains_biases)
 li_refined_shadow_masks = masking.apply_li_threshold_multi(
     shadow_inds, potential_shadow_masks)
@@ -80,15 +80,14 @@ cloud_shadow_masks = np.where(li_refined_shadow_masks == 0, 0, refined_masks)
 print("seconds ", time.time()-start)
 print("finished refined shadow masking")
 
-
 #%%
 import rioxarray
-cloud_shadow_masks
-
-#%%
-import rioxarray
+import xarray as xr
 coords_copy = t_series['reflectance'].drop('band').coords
-masks = xr.DataArray(cloud_shadow_masks, dims=['time', 'y', 'x'], coords=[np.ones(44), np.ones(1082), np.ones(1084)])
+masks = xr.DataArray(cloud_shadow_masks, dims=['time', 'y', 'x'], coords=[np.ones(t_series['reflectance'].shape[0]), np.ones(t_series['reflectance'].shape[1]), np.ones(t_series['reflectance'].shape[2])])
 masks.assign_coords(time=coords_copy['time'], x=coords_copy['x'], y=coords_copy['y'])
+masks = masks.astype(np.int16)
+masks.rio.to_raster("/home/rave/pyatsa/cfg/atsa_results/citymasks.tif")
+
 
 #%%
